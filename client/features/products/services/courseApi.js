fc198ab843e441dbe4da8036b9b92f51b8d7726b
NgoQuoc04 <ngochiquoc140@gmail.com>
2026-06-18 17:48:51 +0700

    chore: completely remove unused Redux Toolkit and react-redux dependencies

--- client/features/products/services/courseApi.js
+++
@@ -1,7 +0,0 @@
-// features/products/services/courseApi.js
-// API calls riêng cho feature Products/Courses
-
-import { courseServices } from "@/services/courseServices";
-
-export const getCoursesApi = (query) => courseServices.getCourses(query);
-export const getCourseBySlugApi = (slug) => courseServices.getCourseBySlug(slug);
